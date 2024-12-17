14e56ed70e455a95ebb7aac8e936a133cd979dd4
anhlinh <gabrielle.piketty@ensae.fr>
2024-12-17 10:36:29 +0000

    modif repro

--- data/visualisation.py
+++ data/visualisation.py
@@ -4,9 +4,20 @@ import geopandas as gpd
 import os
 import zipfile
 import numpy as np
-import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 import seaborn as sns
+import s3fs
+
+from scipy.signal import savgol_filter
+
+import matplotlib.dates as mdates
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+import matplotlib.colors as colors
+from IPython.display import Image, display
+
+
 
 # Définir la charte graphique pour qu'elle soit uniforme entre les différentes stats descriptives (on veut que nos graphiques matchent avec nos cartes)
 charte_graphique = {
@@ -36,7 +47,8 @@ charte_graphique2 = {
 
 def tracer_evolution_taux(
     df, charte_graphique, taux="Taux (/10 000)", title="Évolution des taux d'infractions", 
-    xlabel="Date", ylabel="Nombre d'occurence pour 10 000 habitants", use_log_scale=False, smooth=False, window_size=100, time_period=[]
+    xlabel="Date", ylabel="Nombre d'occurence pour 10 000 habitants", use_log_scale=False, 
+    smooth=False, window_size=100, time_period=[]
 ):
     """
     Trace l'évolution des taux pour une liste d'indicateurs, avec option de lissage.
@@ -51,20 +63,33 @@ def tracer_evolution_taux(
         use_log_scale (bool): Utiliser une échelle logarithmique sur l'axe des y.
         smooth (bool): Lisser les courbes avec une moyenne mobile.
         window_size (int): Taille de la fenêtre pour la moyenne mobile.
+        time_period (list): Liste avec [date_debut, date_fin] pour filtrer les données temporellement.
     """
     plt.figure(figsize=(14, 7))
-    
+
+    # Vérifier et convertir la colonne 'Date' en datetime si nécessaire
+    if not pd.api.types.is_datetime64_any_dtype(df['Date']):
+        df['Date'] = pd.to_datetime(df['Date'], errors='coerce')  # 'coerce' transforme les erreurs en NaT
+
     for indicateur, couleur in charte_graphique.items():
         # Filtrer les données pour l'indicateur
         filtre = df['Indicateur'] == indicateur
         
         # Appliquer borne temporelle si demandé :
         if time_period != []:
-        # Appliquer d'abord le filtre sur l'indicateur, puis appliquer la borne temporelle
-            dates = df.loc[(df['Date'] >= time_period[0]) & (df['Date'] <= time_period[1]) & filtre, 'Date']
-            valeurs = df.loc[(df['Date'] >= time_period[0]) & (df['Date'] <= time_period[1]) & filtre, taux]
+            # Appliquer d'abord le filtre sur l'indicateur, puis appliquer la borne temporelle
+            dates = df.loc[
+                (df['Date'] >= pd.to_datetime(time_period[0])) & 
+                (df['Date'] <= pd.to_datetime(time_period[1])) & 
+                filtre, 'Date'
+            ]
+            valeurs = df.loc[
+                (df['Date'] >= pd.to_datetime(time_period[0])) & 
+                (df['Date'] <= pd.to_datetime(time_period[1])) & 
+                filtre, taux
+            ]
         else:
-        # Si pas de borne temporelle, appliquer seulement le filtre sur l'indicateur
+            # Si pas de borne temporelle, appliquer seulement le filtre sur l'indicateur
             dates = df.loc[filtre, 'Date']
             valeurs = df.loc[filtre, taux]
 
@@ -90,11 +115,13 @@ def tracer_evolution_taux(
     plt.tight_layout()
     plt.show()
 
-from scipy.signal import savgol_filter
-
-def tracer_evolution_taux_relatif_lisse(df, charte_graphique, title="Évolution des taux d'infractions (Indice 1 en 1996)", xlabel="Date", ylabel="Taux normalisé"):
+def tracer_evolution_taux_relatif_lisse(
+    df, charte_graphique, 
+    title="Évolution des taux d'infractions (Indice 1 en 1996)", 
+    xlabel="Date", ylabel="Taux normalisé"
+):
     """
-    Trace l'évolution des taux normalisés pour une liste d'indicateurs.
+    Trace l'évolution des taux normalisés pour une liste d'indicateurs avec lissage.
 
     Args:
         df (pd.DataFrame): Le dataframe contenant les données.
@@ -103,27 +130,33 @@ def tracer_evolution_taux_relatif_lisse(df, charte_graphique, title="Évolution 
         xlabel (str): Label de l'axe des x.
         ylabel (str): Label de l'axe des y.
     """
-
     window_length = 200
     polyorder = 5
 
     plt.figure(figsize=(14, 7))
+
+    # Vérifier et convertir la colonne 'Date' en datetime si nécessaire
+    if not pd.api.types.is_datetime64_any_dtype(df['Date']):
+        df['Date'] = pd.to_datetime(df['Date'], errors='coerce')  # Convertir, remplacer erreurs par NaT
 
     for indicateur, couleur in charte_graphique.items():
         # Extraire les données pour l'indicateur
         filtre = df['Indicateur'] == indicateur
         data_indicateur = df.loc[filtre].copy()
 
+        # Trier les données par date pour garantir le bon affichage
+        data_indicateur = data_indicateur.sort_values(by='Date').reset_index(drop=True)
 
-        # Normaliser les valeurs en divisant par la valeur initiale (1996)
+        # Normaliser les valeurs en divisant par la valeur initiale (première valeur)
         valeur_initiale = data_indicateur.iloc[0]['Taux (/10 000)']
         data_indicateur['Taux normalisé'] = data_indicateur['Taux (/10 000)'] / valeur_initiale
-        #data_indicateur = data_indicateur[(data_indicateur['Date'] >= "2019-01-01") & (data_indicateur['Date'] <= "2021-01-01")]
-        # Appliquer un lissage aux taux relatifs avec Savitzky-Golay
+
+        # Appliquer un lissage aux taux normalisés avec Savitzky-Golay
         if len(data_indicateur) >= window_length:
-            taux_lisse = savgol_filter(data_indicateur['Taux relatif'], window_length=window_length, polyorder=polyorder)
+            taux_lisse = savgol_filter(data_indicateur['Taux normalisé'], window_length=window_length, polyorder=polyorder)
         else:
-            taux_lisse = data_indicateur['Taux relatif']  # Pas de lissage si pas assez de points
+            taux_lisse = data_indicateur['Taux normalisé']  # Pas de lissage si pas assez de points
+
         # Tracer la courbe
         plt.plot(
             data_indicateur['Date'], 
@@ -144,9 +177,94 @@ def tracer_evolution_taux_relatif_lisse(df, charte_graphique, title="Évolution 
     plt.tight_layout()
     plt.show()
 
-# Ajouter une colonne 'Saison' au dataframe en fonction du mois
-def get_saison(mois):
-    for saison, mois_list in saisons.items():
-        if mois in mois_list:
-            return saison
-    return None  # Retourner None si le mois n'est pas trouvé (devrait pas arriver)
+# Fonction pour tracer le boxplot avec couleurs personnalisées pour chaque saison
+def boxplot_indicateur_par_saison(df, indicateur):
+    # Ajouter la colonne 'Saison' au dataframe
+    
+    # Filtrer les données pour l'indicateur spécifié
+    df_indicateur_filtre = df[df['Indicateur'] == indicateur]
+    
+    # Définir les couleurs pour chaque saison
+    saison_colors = {
+        'Hiver': 'cornflowerblue',  # Bleu clair pour l'hiver
+        'Printemps': 'limegreen',  # Vert clair pour le printemps
+        'Été': 'gold',  # Jaune orangé pour l'été
+        'Automne': 'brown'  # Marron-rouge pour l'automne
+    }
+    
+    # Créer le boxplot avec la palette de couleurs définie
+    plt.figure(figsize=(10, 6))
+    sns.boxplot(x='Saison', y='Taux (/10 000)', data=df_indicateur_filtre, 
+                palette=saison_colors)
+    
+    # Ajouter un titre et des labels
+    plt.title(f'Boxplot du taux de {indicateur} selon les saisons')
+    plt.xlabel('Saison')
+    plt.ylabel('Taux (/10 000)')
+    
+    # Afficher le plot
+    plt.show()
+
+
+def évolution_indicateur(indicateur):
+    # Définir le nombre de cartes par ligne
+    cartes_par_ligne = 7
+    
+    # Calculer le nombre de lignes nécessaires
+    total_annees = 2022 - 1996 + 1
+    lignes = (total_annees // cartes_par_ligne) + (1 if total_annees % cartes_par_ligne != 0 else 0)
+    
+    # Créer une figure avec une taille ajustée pour les petites cartes
+    fig, axes = plt.subplots(nrows=lignes, ncols=cartes_par_ligne, figsize=(12, lignes * 2))
+    
+    # Aplatir la liste des axes pour y accéder plus facilement
+    axes = axes.flatten()
+
+    # Créer une liste pour stocker les valeurs des taux pour l'échelle partagée
+    taux_values = []
+
+    # Parcours des années de 1996 à 2022
+    for idx, annee in enumerate(range(1996, 2023)):
+        # Filtrer les données pour l'indicateur et l'année en cours
+        df_filtre = df_indicateurs_dep[(df_indicateurs_dep['Année'] == str(annee)) & (df_indicateurs_dep['Indicateur'] == indicateur)]
+        
+        # Créer un GeoDataFrame avec la colonne 'Géométrie'
+        gdf = gpd.GeoDataFrame(df_filtre, geometry='Géométrie')
+        
+        # Ajouter les valeurs des taux dans la liste pour l'échelle partagée
+        taux_values.extend(gdf['Taux (/10 000)'].dropna().tolist())
+        
+        # Vérifier si le GeoDataFrame n'est pas vide
+        if not gdf.empty:
+            # Afficher la carte avec le taux (%) sur l'axe correspondant
+            gdf.plot(column='Taux (/10 000)', cmap=charte_graphique2.get(f'{indicateur}'), ax=axes[idx], legend=False)
+            
+            # Titre de la carte
+            axes[idx].set_title(f"{annee}", fontsize=6)
+            axes[idx].axis("off")  # Enlever les axes
+            axes[idx].set_aspect(1.4)
+        else:
+            axes[idx].axis("off")  # Si aucune donnée, ne pas afficher d'axes
+
+    # Créer un ScalarMappable pour la légende partagée
+    sm = plt.cm.ScalarMappable(cmap=charte_graphique2.get(f'{indicateur}'), norm=mpl.colors.Normalize(vmin=min(taux_values), vmax=max(taux_values)))
+    sm.set_array([])  # Nécessaire pour la légende
+    
+    # Ajouter la légende commune à la dernière case vide (celle qui reste vide)
+    cbar = fig.colorbar(sm, 
+                        ax=axes[-1], 
+                        orientation='horizontal', 
+                        fraction=0.045, 
+                        pad=0.06, 
+                        label="Occurences pour \n 10 000 habitants")
+
+    # Supprimer le contour de la dernière case où la légende est placée
+    axes[-1].axis("off")
+
+    fig.suptitle(f"{indicateur}", fontsize=13, fontweight='bold', y=0.68) # titre commun
+
+    # Ajuster l'espacement pour les sous-graphes et la légende
+    plt.subplots_adjust(hspace=0.1, bottom=0, top=0.6)  # Ajuster l'espacement vertical et l'espacement en bas
+    
+    # Afficher la figure
+    plt.show()
